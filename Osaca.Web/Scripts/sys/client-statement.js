d1189e8add867448023309970edec0f1a73e8296
M. Salah <eng.msalah.abdullah@gmail.com>
2017-05-16 16:51:30 +0400

    fix bug

--- Osaca.Web/Scripts/sys/client-statement.js
+++ Osaca.Web/Scripts/sys/client-statement.js
@@ -25,21 +25,25 @@ var
                     BindReportControls = function (d) {
                         var data = commonManger.xml2Json(d.d),
                             jsn = data.list,
-                            jsn1 = data.list1;
-                        
+                            jsn1 = data.list1,
+                            latestBalance = 0;
+
                         // header details
                         $('#AddDate').text(commonManger.formatJSONDateCal(new Date(), 'dd/MM/yyyy'));
                         if (jsn) {
                             $('#ClientName').text(jsn.ClientName);
-                            $('.FinalBalance').text(numeral(jsn.DuAmount).format('0,0.00'));
                         }
-                        
+
                         // statement details
                         var rows = $(jsn1).map(function (i, v) {
+                            latestBalance = v.Balance;
                             return `<tr><td class="center">${v.Note === 'Invoice' ? '<a title="Go to invoice details" target="_blank" href="InvoicePrint.aspx?id=' + v.ID + '">' + v.ID + '</a>' : v.ID}</td><td>${commonManger.formatJSONDateCal(v.Date, 'dd/MM/yyyy')}</td><td>${numeral(v.InAmount).format('0,0.00')}</td><td>${numeral(v.OutAmount).format('0,0.00')}</td><td>${numeral(v.Balance).format('0,0.00')}</td></tr>`;
                         }).get().join(),
                             $stet = $('.listItems tbody');
                         $stet.html(rows);
+
+                        // show final balance.
+                        $('.FinalBalance').text(numeral(latestBalance).format('0,0.00'));
                     };
 
                 dataService.callAjax('Post', JSON.stringify(DTO), sUrl + 'GetData',
